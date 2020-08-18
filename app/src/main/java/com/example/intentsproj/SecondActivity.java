981f0da9d05e76c95f96c8c6b174454b814ec5b0
HP <it19057170@my.sliit.lk>
2020-08-18 22:01:09 +0530

    Initial Tutorial 03

---
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.intentsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
